96f2d4e6ca4f808cce571fe89e4259bcd31679c0
Doe Mohamud <doe.mahamud1@gmail.com>
2021-07-19 11:49:44 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,13 +6,15 @@ class PostsController < ApplicationController
 
   def edit
   end
-
   def update
     @post.update(post_params)
-
-    redirect_to post_path(@post)
+    if @post.valid?
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
   end
-
   private
 
   def post_params
